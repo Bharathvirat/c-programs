eac727c5c0c399d47841eff91a6ae81415aaa0ff
Bharathvirat <61828088+Bharathvirat@users.noreply.github.com>
2020-03-06 10:12:45 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 9;
+  int b = 4;
+  
+  printf(" a >  b: %d \n", a > b);
+  printf("a >= b: %d \n", a >= b);
+  printf("a <= b: %d \n", a <= b);
+  printf("a <  b: %d \n", a < b);
+  printf("a == b: %d \n", a == b);
+  printf("a != b: %d \n", a != b);
+
+}
